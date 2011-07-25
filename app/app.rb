56fdc8c1daf1f19246dc1c3d91b815bc2d53617b
Ken Erickson <bookworm.productions@gmail.com>
2011-07-25 15:39:35 -0700

    fixes

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,11 @@ class Me < Padrino::Application
   register Padrino::Helpers
   register Padrino::Rendering   
   register CompassInitializer
-  register AssetHatInitializer
-  register Padrino::Cache   
-  
-  enable :caching 
-  
-  set :cache, Padrino::Cache::Store::Redis.new(::Redis.new)
+  register AssetHatInitializer      
+  # For Now Disable Cache
+  # register Padrino::Cache   
+  # 
+  # enable :caching if Padrino.env == :production
+  # 
+  # set :cache, Padrino::Cache::Store::Redis.new(::Redis.new)
 end
