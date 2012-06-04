0a42a295b2f36e8eb384268b78a2b2f850dd983d
Ken Erickson <bookworm.productions@gmail.com>
2012-06-04 15:51:01 -0700

    compass plugin cleanup

--- lib/compass_plugin.rb
+++ lib/compass_plugin.rb
@@ -1,34 +1,22 @@
 # Enables support for Compass, a stylesheet authoring framework based on SASS.
 # See http://compass-style.org/ for more details.
-# Store Compass/SASS files (by default) within 'app/stylesheets'
-
+# Store Compass/SASS files (by default) within 'app/stylesheets'  
 module CompassInitializer
-  def self.registered(app)
-    require 'sass/plugin/rack'
+  def self.registered(app)   
+    require 'sass/plugin/rack'   
+    require 'bootstrap-sass'
 
     Compass.configuration do |config|
-      config.project_path = Padrino.root
-      config.sass_dir = "app/stylesheets"
-      config.project_type = :stand_alone
-      config.http_path = "/"
-      config.css_dir = "public/stylesheets"
-      config.images_dir = "public/images"      
-      config.javascripts_dir = "public/javascripts"   
-      config.output_style = :compressed    
-      config.http_images_path = config.http_path + "images"
+      config.project_path    = Padrino.root
+      config.sass_dir        = "app/assets/css"
+      config.project_type    = :stand_alone
+      config.http_path       = "/"
+      config.css_dir         = "public/css"
+      config.images_dir      = "public/images"
+      config.javascripts_dir = "public/js"
+      config.output_style    = :compressed
+    end  
 
-      if Padrino.env == :production   
-        config.http_path = "http://assets0." + ENV['DOMAIN'] 
-        config.images_dir = "public/images"          
-        config.http_images_path = "/" + 'images'
-        config.relative_assets  = false   
-        config.asset_host do |asset|
-          "http://assets%d.#{ENV['DOMAIN']}" % (asset.hash % 4)
-        end        
-        config.output_style = :compressed     
-      end
-    end
-    
     Compass.configure_sass_plugin!
     Compass.handle_configuration_change!
 
