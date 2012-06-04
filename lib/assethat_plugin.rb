818bed3a6951306fe4715365d91a69cbcf35245a
Ken Erickson <bookworm.productions@gmail.com>
2012-06-04 15:49:10 -0700

    remove assethat lib

--- lib/assethat_plugin.rb
+++
@@ -1,9 +0,0 @@
-module AssetHatInitializer
-  def self.registered(app)     
-    require 'asset_hat.rb'
-    require 'asset_hat_helper'   
-    require 'asset_hat_sinatra' 
-    app.register AssetHat::Sinatra      
-    
-	end  
-end
