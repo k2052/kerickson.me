eb45534061a821d06afc9eb6b868df6e4a16e06b
Ken Erickson <bookworm.productions@gmail.com>
2012-06-04 16:09:08 -0700

    cleanup app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,29 @@
-class Me < Padrino::Application
-  register Padrino::Helpers
-  register Padrino::Rendering   
-  register CompassInitializer
-  register AssetHatInitializer      
+require 'padrino/sprockets'   
+class Me < Padrino::Applicatio
+  use Airbrake::Rack     
+  register Padrino::Rendering  
   register Padrino::Cache   
-  
+  register Padrino::Helpers   
+  register CompassInitializer
+
   enable :caching if Padrino.env == :production
+  set :cache, Padrino::Cache::Store::Redis.new(::Redis.new)            
   
-  set :cache, Padrino::Cache::Store::Redis.new(::Redis.new)
+  ## 
+  # Assets
+  #  
+  set :stylesheets_folder, :css
+  set :javascripts_folder, :js
+  register Padrino::AssetHelpers
+  register Padrino::Sprockets   
+  register Padrino::Responders
+  
+  assets do    
+    digest false  
+    handle_stylesheets false  
+    assets_folder '/public'
+    append_path 'assets/js'  
+    append_path '../lib/assets/js'
+    append_path '../vendor/assets/js'   
+  end  
 end
