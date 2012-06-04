1605b85db94aa3fd5f98dce3520a368011ca238b
Ken Erickson <bookworm.productions@gmail.com>
2012-06-04 16:32:14 -0700

    drop old tests

--- test/test_config.rb
+++ test/test_config.rb
@@ -1,13 +1,14 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
-require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+require File.expand_path('../../config/boot', __FILE__)
 
-class Test::Unit::TestCase
+class MiniTest::Unit::TestCase
   include Rack::Test::Methods
 
   def app
     ##
     # You can handle all padrino applications using instead:
     #   Padrino.application
-    Me.tap { |app|  }
+    Me.tap { |app|  }         
   end
 end
+
